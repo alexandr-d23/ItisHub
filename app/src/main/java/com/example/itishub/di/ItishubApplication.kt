283a258b4747c33f55da1ce51bb388870c63af98
alexandr-d23 <alexandr-d23@mail.ru>
2021-03-26 13:45:40 +0300

    skeleton

--- app/src/main/java/com/example/itishub/di/ItishubApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.itishub.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ItishubApplication : Application(){
-}
--- app/src/main/java/com/example/itishub/presenter/adapters/LessonAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.itishub.presenter.adapters
-
-class LessonAdapter {
-}
